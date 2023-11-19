b9198b4bd4583861428ae5521a039b20f72612ac
Zero <zero0396@gmail.com>
2023-11-18 22:42:31 -0400

    consolidation

--- FactsAPI.py
+++ FactsAPI.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 import json
 import random
+from starlette.responses import RedirectResponse
 app = FastAPI()
 
-version = "1.0.2"
+version = "1.0.3"
 base_url = '/api/v1'
 
 #import json file
@@ -21,92 +22,95 @@ ufacts = ufacts['facts']
 ufacts_size = len(ufacts)
 
 
-
-
 @app.get('/')
 def read_root():
-    return {'Hello': 'World'}
+    # redirect to /docs url
+    return RedirectResponse(url='/docs')
 
-@app.get(base_url+'/useless/fact')
-def getUFact():
-    try:
-
-        random_index = random.randrange(0,ufacts_size - 1)
-        return {
-                'id':random_index,
-                'fact': ufacts[random_index]['text'],
-                'source':ufacts[random_index]['source']
-                }
-
-    except Exception as e:
-        print(e)
-        return {
-            'error': 'Unable to process request',
-            }
-
-
-@app.get(base_url+'/useless/fact/{useless_fact_id}')
-def getUFactWithId(useless_fact_id: int):
-    try:
-
-        if useless_fact_id < 0 or useless_fact_id > ufacts_size - 1:
-            return {
-                'error': 'Fact ID out of range',
-                }
-        else:
-            return {
-                'id':useless_fact_id,
-                'fact': ufacts[useless_fact_id]['text'],
-                'source':ufacts[useless_fact_id]['source']
-                }
-
-    except Exception as e:
-        print(e)
-        return {
-            'error': 'Unable to process this request',
-            }
-
-
-@app.get(base_url+'/intresting/fact')
-def getIFact():
-    try:
-
-        random_index = random.randrange(0,ifacts_size - 1)
-        return {
-                'id':random_index,
-                'fact': ifacts[random_index],
-                'source':"N/A"
-                }
-
-    except Exception as e:
-        print(e)
-        return {
-            'error': 'Unable to process request',
-            }
-
-
-@app.get(base_url+'/intresting/fact/{intresting_fact_id}')
-def getIFactWithId(intresting_fact_id: int):
-    try:
-
-        if intresting_fact_id < 0 or intresting_fact_id > ifacts_size - 1:
-            return {
-                'error': 'Fact ID out of range',
-                }
-        else:
-            return {
-                'id':intresting_fact_id,
-                'fact': ifacts[intresting_fact_id],
-                'source':"N/A"
-                }
-
-    except Exception as e:
-        print(e)
-        return {
-            'error': 'Unable to process this request',
-            }
 @app.get('/version')
 def getVersion():
     return {
         'version': version
         }
+
+@app.get(base_url+'/useless/fact')
+def getUFact(fid:str | None = None ):
+    try:
+
+        if fid == None:
+            fid = ""
+
+        if fid.isdigit():
+            fid = int(fid)
+            if fid <= 0 or fid > ufacts_size - 1 :
+                return {
+                    'error': 'Fact ID out of range',
+                }
+            else:
+                return {
+                    'id':fid,
+                    'fact': ufacts[fid]['text'],
+                    'source':ufacts[fid]['source']
+                }
+        elif fid.isdigit() == False and fid != "":
+            return {
+                'error': 'Fact ID must be a number',
+            }
+        elif fid == "":
+                        #check for query string fid
+            random_index = random.randrange(0,ufacts_size - 1)
+            return {
+                    'id':random_index,
+                    'fact': ufacts[random_index]['text'],
+                    'source':ufacts[random_index]['source']
+                    }
+        else:
+            return {
+                'error': 'Unable to process request',
+            }
+
+    except Exception as e:
+        print(e)
+        return {
+            'error': 'Unable to process request',
+            }
+
+
+@app.get(base_url+'/intresting/fact')
+def getIFact(fid:str | None = None):
+    try:
+        if fid == None:
+            fid = ""
+        if fid.isdigit():
+            fid = int(fid)
+            if fid <= 0 or fid > ifacts_size - 1 :
+                return {
+                    'error': 'Fact ID out of range',
+                }
+            else:
+                return {
+                    'id':fid,
+                    'fact': ifacts[fid],
+                    'source':"N/A"
+                }
+        elif fid.isdigit() == False and fid != "":
+            return {
+                'error': 'Fact ID must be a number',
+            }
+        elif fid == "":
+            random_index = random.randrange(0,ifacts_size - 1)
+            return {
+                    'id':random_index,
+                    'fact': ifacts[random_index],
+                    'source':"N/A"
+                    }
+        else:
+            return {
+                'error': 'Unable to process request',
+            }
+
+    except Exception as e:
+        print(e)
+        return {
+            'error': 'Unable to process request',
+            }
